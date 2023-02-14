0fc26f61cdd005499d77d6e6c7b6778e3ccd179a
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-02-14 14:15:09 +0530

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://cutstyle.true-emotions.studio/dark-beard</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cutstyle.true-emotions.studio/dark-beard/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cutstyle.true-emotions.studio/dark-beard/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cutstyle.true-emotions.studio/dark-beard/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cutstyle.true-emotions.studio/dark-beard/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cutstyle.true-emotions.studio/dark-beard/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
